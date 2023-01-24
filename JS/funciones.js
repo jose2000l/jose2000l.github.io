fdd703af95eb2d8fde663916fa6a344d9d6f991d
EL licenciado <jose2000labanda@gmail.com>
2023-01-24 10:31:37 -0300

    container autoadaptable con js

---
+++ JS/funciones.js
@@ -0,0 +1,23 @@
+
+/** Descripcion: Capturamos el evento: El mouse está sobre el boton enviar del formulario */
+document.getElementById("btnEnviar").addEventListener('mouseover',function(){
+    this.style.backgroundColor='green';
+});
+/** Descripcion: Capturamos el evento: El mouse salio del boton enviar del formulario */
+document.getElementById("btnEnviar").addEventListener('mouseout',function(){
+   this.style.backgroundColor='buttonface';
+});
+
+/** Descripcion: Capturamos el evento: Se redimensionó la pantalla */
+window.addEventListener('resize',function(){
+    var widthNew = document.documentElement.clientWidth;
+    redimensionar(widthNew);
+    
+});
+/**Descripcion: Redimensiona la barra lateral 50px  menos que el ancho de la pantalla */
+function redimensionar(anchoPantalla){
+    var nuevoLargo = anchoPantalla-50;
+    var StrnuevoLargo = nuevoLargo+"px";
+   
+    document.getElementById("barLateral").style.width=StrnuevoLargo;
+}
